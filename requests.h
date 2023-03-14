1137dd9f937daea76c5e4591edee5f5b0d43357d
anamariasuiu <74262373+anamariasuiu@users.noreply.github.com>
2023-03-14 12:49:52 +0200

    Add files via upload

---
+++ requests.h
@@ -0,0 +1,16 @@
+#ifndef _REQUESTS_
+#define _REQUESTS_
+
+// computes and returns a GET request string (query_params
+// and cookies can be set to NULL if not needed)
+char *compute_get_request(const char *host, const char *url, const char *query_params,
+							char **cookies, int cookies_count, const char *token);
+
+// computes and returns a POST request string (cookies can be NULL if not needed)
+char *compute_post_request(const char *host, const char *url, char* content_type, char **body_data,
+							int body_data_fields_count, char** cookies, int cookies_count, const char* token);
+
+char *compute_delete_request(const char *host, const char *url,const char *query_params,
+                            char **cookies, int cookies_count,const char *token);
+
+#endif
